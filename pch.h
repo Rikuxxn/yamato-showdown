38fb800ac232bf08a20cea7d3e1c012e44c1ccb5
種川陸 <241312@st.yoshida-g.ac.jp>
2025-10-24 08:47:33 +0900

    s

--- pch.h
+++ pch.h
@@ -49,7 +49,7 @@
 // マクロ定義
 //*****************************************************************************
 #define CLASS_NAME	  "WindowClass"								// ウィンドウクラスの名前
-#define WINDOW_NAME	  "Ruins Good!"								// ウィンドウの名前(キャプションに表示)
+#define WINDOW_NAME	  "YAMATO Showdown"							// ウィンドウの名前(キャプションに表示)
 #define SCREEN_WIDTH  (1280)									// ウィンドウの幅
 #define SCREEN_HEIGHT (720)										// ウィンドウの高さ
 #define FVF_VERTEX_2D (D3DFVF_XYZRHW|D3DFVF_DIFFUSE|D3DFVF_TEX1)
